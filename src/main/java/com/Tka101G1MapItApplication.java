0eb363344285140c21f332cf1ba76748c51362b3
Jason Zheng <jason.zheng0115@gmail.com>
2026-06-16 16:54:20 +0800

    first commit

---
+++ src/main/java/com/Tka101G1MapItApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tka101G1MapItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tka101G1MapItApplication.class, args);
+	}
+
+}
