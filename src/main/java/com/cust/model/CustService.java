1bc71af67e97fd7c0ba7b7f9e4adfe60f7974a33
10 Chen <style226655@gmail.com>
2026-06-19 16:19:00 +0800

    會員功能-ver.5

--- src/main/java/com/cust/model/CustService.java
+++ src/main/java/com/cust/model/CustService.java
@@ -1,13 +1,21 @@
 package com.cust.model;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
 
 @Service //貼標籤，讓SpringBoot確認接管功能
 public class CustService {
 	
 	@Autowired //讓springBoot自己new，要拿CustRepository實作
 	private CustRepository repository;
+	
+	@Autowired //自動注入EntityManager，並且是去Composite
+	private EntityManager entityManager;
 	
 	//會員登入
 	public CustVO login(String account, String password) {
@@ -16,7 +24,7 @@ public class CustService {
 	}
 	//會員註冊
 	public void register(CustVO custVO) {
-		CustVO existId = repository.findByAccount(custVO.getCust_id());
+		CustVO existId = repository.findByAccount(custVO.getCust_account());
 		if (existId != null) {
 			throw new RuntimeException("該帳號已被註冊，請換一個帳號");
 		}
@@ -28,6 +36,11 @@ public class CustService {
 		System.out.println("更新的會員名字：" + custVO.getCust_name());
 		repository.save(custVO); //custVO沒有ID傳進來時，會自動INSERT;custVO有ID時，會自動寫UPDATE
 		}
+	
+	//將CustCompositeQuery的條件，用EntityManger把資料庫的資料撈出來
+	public List<CustVO> getAll(Map<String,String[]> map){
+		return CustCompositeQuery.getAllC(map, entityManager);
+	}
 	
 	
 }
