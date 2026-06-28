b754ab579d1f1379f426885274c90d51fe2b3f8b
Jason Zheng <jason.zheng0115@gmail.com>
2026-06-28 18:06:18 +0800

    6/28

--- src/main/java/com/index/controller/IndexController.java
+++ src/main/java/com/index/controller/IndexController.java
@@ -14,7 +14,7 @@ public class IndexController {
         // 1. 嘗試從伺服器的「記憶保險箱 (Session)」中，拿出名為 "loginCust" 的資料
         CustVO loginCust = (CustVO) session.getAttribute("loginCust");
 
-        // 2. 判斷保險箱裡有沒有這個人（是不是已經登入了？）
+        // 2. 判斷session裡有沒有custVO這個物件（是否已經登入了？）
         if (loginCust != null) {
             // 3. 如果有登入，就把他的名字拿出來，裝進準備送到前端的包裹 (Model) 裡面
             // 並在包裹上貼個標籤叫做 "userName"
