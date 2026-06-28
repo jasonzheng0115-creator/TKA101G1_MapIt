7b898a4ab01a3ab695311262bd17505b839e7655
H.H.Lin <gecko0995@gmail.com>
2026-06-28 22:29:41 +0800

    order_management update

--- src/main/java/com/cust/filter/LoginFilter.java
+++ src/main/java/com/cust/filter/LoginFilter.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@Component
+//@Component
 public class LoginFilter extends OncePerRequestFilter {
 
 	@Override
