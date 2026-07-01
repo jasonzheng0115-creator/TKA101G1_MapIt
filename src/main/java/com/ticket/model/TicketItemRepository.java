e2bcb6ee93d59ef028ce2aad313c15828e57d1ad
H.H.Lin <gecko0995@gmail.com>
2026-07-02 01:22:30 +0800

    revised Orders and Ticket for cancel

--- src/main/java/com/ticket/model/TicketItemRepository.java
+++ src/main/java/com/ticket/model/TicketItemRepository.java
@@ -1,5 +1,6 @@
 package com.ticket.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,8 @@ public interface TicketItemRepository extends JpaRepository<TicketItemVO, Ticket
 	// 根據tktId找出明細
 	@Query("SELECT ticket FROM TicketItemVO ticket WHERE ticket.ticketId.tktId = ?1")
 	TicketItemVO findByTktId(Integer tktId);
+	
+	// === 精準查詢發票明細的方法 ===
+	List<TicketItemVO> findByCustId_CustIdAndStartDate(Integer custId, LocalDateTime startDate);
 
 }
